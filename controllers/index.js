cc55ceb0a9f656834684e70452287cfdc17337bd
Luis A. Acurero <13luismb@gmail.com>
2018-11-26 21:30:00 -0400

    basic fileupload logic

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,5 @@ let router = express.Router();
 
 router.use('/session', require('./session'));
 router.use('/register', require('./register'));
+router.use('/content', require('./content'));
 module.exports = router;
