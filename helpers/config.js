5df13a9ac609658cac97a31e2e48e614bb933f1d
Luis A. Acurero <13luismb@gmail.com>
2018-11-30 02:57:42 -0400

    more client logic

--- helpers/config.js
+++ helpers/config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: 'postgres://postgres:admin@localhost:5432/Projectmanager',
+    dbUrl: 'postgres://postgres:2583955@localhost:5432/Projectmanager',
     port: 3001,
     secret: 'Vu1Kn0'
 }
--- public/js/items.js
+++ public/js/items.js
@@ -1,16 +1,16 @@
-function $(id){
+function $(id) {
     return document.getElementById(id);
 }
 
 const project_id = getIdProject();
 
-function getIdProject(){
+function getIdProject() {
     var url_string = window.location.href;
     var url = new URL(url_string);
     var id = url.searchParams.get("project_id");
-    if(id === null || undefined){
+    if (id === null || undefined) {
         location.href = "dashboard.html";
-    }else{
+    } else {
         return id;
     }
 }
@@ -21,70 +21,70 @@ function getIdProject(){
                     */
 
 window.onload = function() {
-    if(sessionStorage.getItem("token")==null||undefined){
+    if (sessionStorage.getItem("token") == null || undefined) {
         alert("NO TIENES SESSION!");
         location.href = "dashboard.html";
-    }else{
+    } else {
         let config = {
             method: "GET",
             withCredentials: true,
             credentials: 'same-origin',
             headers: {
                 "Content-type": "application/x-www-form-urlencoded",
-                "Authorization":"Bearer "+sessionStorage.getItem("token")
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
             }
         };
-        
+
         let params = location.href.split('?')[1];
-        fetch('./../item/getListItem?'+params, config)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.status == 200 || data.status == 201){
-                data.data.forEach(element => {
-                    var status = getDataStatus(element.status_id);
-                    console.log(status);
-                    $('collapsible').innerHTML += ""+
-                    '<li id='+element.project_item_id+' onclick="getContent('+element.project_item_id+')">'+
-                    '<div class="collapsible-header">'+element.project_item_name+'<span class="new badge '+status.color+'" data-badge-caption="'+status.msg+'"></span></div>'+
-                    '<div class="collapsible-body white-text" id="item-'+element.project_item_id+'">'+
-                    '<span>'+element.project_item_description+'</span>'+
-                    '</div>'+
-                    '</li>';    
-                });
-            } else{
-                alert(data.message+" Error:"+data.status);
-                location.href = "dashboard.html";
-            }
-        });
+        fetch('./../item/getListItem?' + params, config)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.status == 200 || data.status == 201) {
+                    data.data.forEach(element => {
+                        var status = getDataStatus(element.status_id);
+                        console.log(status);
+                        $('collapsible').innerHTML += "" +
+                            '<li id=' + element.project_item_id + ' onclick="getContent(' + element.project_item_id + ')">' +
+                            '<div class="collapsible-header">' + element.project_item_name + '<span class="new badge ' + status.color + '" data-badge-caption="' + status.msg + '"></span></div>' +
+                            '<div class="collapsible-body white-text" id="item-' + element.project_item_id + '">' +
+                            '<span>' + element.project_item_description + '</span>' +
+                            '</div>' +
+                            '</li>';
+                    });
+                } else {
+                    alert(data.message + " Error:" + data.status);
+                    location.href = "dashboard.html";
+                }
+            });
 
         //Cambiar para el admin
         $("project_id_delegado").value = getIdProject();
-    }    
+    }
 }
 
-function getDataStatus(status){
-    
-    switch(status){
-        case 1: 
+function getDataStatus(status) {
+
+    switch (status) {
+        case 1:
             return {
-                msg:"Completed",
-                color:"green"
+                msg: "Completed",
+                color: "green"
             }
         case 2:
             return {
-                msg:"Progess",
-                color:"yellow"
+                msg: "Progess",
+                color: "yellow"
             }
         case 3:
             return {
-                msg:"Started",
-                color:"red"
+                msg: "Started",
+                color: "red"
             }
     }
 }
 
-function getContent(id){
+function getContent(id) {
     console.log(id);
     $(id).onclick = null;
 
@@ -94,66 +94,66 @@ function getContent(id){
         credentials: 'same-origin',
         headers: {
             "Content-type": "application/x-www-form-urlencoded",
-            "Authorization":"Bearer "+sessionStorage.getItem("token")
+            "Authorization": "Bearer " + sessionStorage.getItem("token")
         }
     };
-    
-    fetch('./../content/getContent?item_id='+id, config)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        if(data.status == 200 || data.status == 201){
-            /*data.data.forEach(element => {
-                $('collapsible').innerHTML += ""+
-                '<li id='+element.project_item_id+' onclick="getContent('+element.project_item_id+')">'+
-                '<div class="collapsible-header">'+element.project_item_name+'</div>'+
-                '<div class="collapsible-body white-text" id="item-'+element.project_item_id+'">'+
-                '<span>'+element.project_item_description+'</span>'+
-                '</div>'+
-                '</li>';    
-            });*/
-            $('item-'+id).innerHTML += "<br><br>" +
-            '<a href="#modal-content" class="modal-trigger waves-effect waves-light btn white black-text" id="btn-new" onclick="assignId('+id+')">Add content</a>'+
-            '<a class="waves-effect waves-light btn white black-text" onclick="changeStatus('+id+','+1+')">Finish</a>'+
-            '<a class="waves-effect waves-light btn white black-text" onclick="editItem('+id+')">Edit</a>'+
-            '<a class="waves-effect waves-light btn white black-text" onclick="deleteItem('+id+')">Delete</a>';
-        } else{
-            alert(data.message+" Error:"+data.status);
-            location.href = "dashboard.html";
-        }
-    });
+
+    fetch('./../content/getContent?item_id=' + id, config)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.status == 200 || data.status == 201) {
+                /*data.data.forEach(element => {
+                    $('collapsible').innerHTML += ""+
+                    '<li id='+element.project_item_id+' onclick="getContent('+element.project_item_id+')">'+
+                    '<div class="collapsible-header">'+element.project_item_name+'</div>'+
+                    '<div class="collapsible-body white-text" id="item-'+element.project_item_id+'">'+
+                    '<span>'+element.project_item_description+'</span>'+
+                    '</div>'+
+                    '</li>';    
+                });*/
+                $('item-' + id).innerHTML += "<br><br>" +
+                    '<a href="#modal-content" class="modal-trigger waves-effect waves-light btn white black-text" id="btn-new" onclick="assignId(' + id + ')">Add content</a>' +
+                    '<a class="waves-effect waves-light btn white black-text" onclick="changeStatus(' + id + ',' + 1 + ')">Finish</a>' +
+                    '<a class="waves-effect waves-light btn white black-text" onclick="editItem(' + id + ')">Edit</a>' +
+                    '<a class="waves-effect waves-light btn white black-text" onclick="deleteItem(' + id + ')">Delete</a>';
+            } else {
+                alert(data.message + " Error:" + data.status);
+                location.href = "dashboard.html";
+            }
+        });
 }
 
-function assignId(id){
-    $('id_item_modal').value = id;    
+function assignId(id) {
+    $('id_item_modal').value = id;
 }
 
-function deleteItem(id){
-    let body={
-        items_id: id,
-    },
-        params={
-                method: "DELETE", 
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    "Authorization":"Bearer "+sessionStorage.getItem("token")
-                }), 
-                body:JSON.stringify(body)
-    };
+function deleteItem(id) {
+    let body = {
+            items_id: id,
+        },
+        params = {
+            method: "DELETE",
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
+            }),
+            body: JSON.stringify(body)
+        };
     fetch("./../item/deleteItem", params)
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        if (data.status==200){
-            location.reload();
-        }else{
-            alert("Error al iniciar sesion, status:"+data.status);
-        }
-    });
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            if (data.status == 200) {
+                location.reload();
+            } else {
+                alert("Error al iniciar sesion, status:" + data.status);
+            }
+        });
 }
 
-function addContent(){
-    var url = "./../content/upload/"+$('id_item_modal').value;
+function addContent() {
+    var url = "./../content/upload/" + $('id_item_modal').value;
     console.log(url);
     var formData = new FormData();
     formData.append('file', $("file").files[0]);
@@ -163,7 +163,7 @@ function addContent(){
             body: formData,
             method: 'POST',
             headers: new Headers({
-                "Authorization":"Bearer "+sessionStorage.getItem("token")
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
             })
         })
         .then(resp => resp.json())
@@ -172,72 +172,95 @@ function addContent(){
         });
 }
 
-function changeStatus(id,status){
-    let body={
-        items_id: id,
-        status: status
-    },
-        config={
-                method: "PUT", 
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    "Authorization":"Bearer "+sessionStorage.getItem("token")
-                }), 
-                body:JSON.stringify(body) 
-    };
-    
+function changeStatus(id, status) {
+    let body = {
+            items_id: id,
+            status: status
+        },
+        config = {
+            method: "PUT",
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
+            }),
+            body: JSON.stringify(body)
+        };
+
     fetch('./../item/editItem', config)
-    .then(res => res.json())
-    .then(data => {
-        if(data.status == 200 || data.status == 201){
-            console.log(data);
-            /*data.data.forEach(element => {
-                $('collapsible').innerHTML += ""+
-                '<li id='+element.project_item_id+' onclick="getContent('+element.project_item_id+')">'+
-                '<div class="collapsible-header">'+element.project_item_name+'</div>'+
-                '<div class="collapsible-body white-text" id="item-'+element.project_item_id+'">'+
-                '<span>'+element.project_item_description+'</span>'+
-                '</div>'+
-                '</li>';    
-            });*/
-            location.reload();
-        } else{
-            alert(data.message+" Error:"+data.status);
-            location.href = "dashboard.html";
-        }
-    });
+        .then(res => res.json())
+        .then(data => {
+            if (data.status == 200 || data.status == 201) {
+                console.log(data);
+                /*data.data.forEach(element => {
+                    $('collapsible').innerHTML += ""+
+                    '<li id='+element.project_item_id+' onclick="getContent('+element.project_item_id+')">'+
+                    '<div class="collapsible-header">'+element.project_item_name+'</div>'+
+                    '<div class="collapsible-body white-text" id="item-'+element.project_item_id+'">'+
+                    '<span>'+element.project_item_description+'</span>'+
+                    '</div>'+
+                    '</li>';    
+                });*/
+                location.reload();
+            } else {
+                alert(data.message + " Error:" + data.status);
+                location.href = "dashboard.html";
+            }
+        });
 }
 
-function createItem(){
-    let body={
-        project_id: project_id,
-        items_name:$('name_item').value,
-        items_des:$('des_item').value
-    },
-        params={
-                method: "POST", 
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    "Authorization":"Bearer "+sessionStorage.getItem("token")
-                }), 
-                body:JSON.stringify(body)
-    };
+function createItem() {
+    let body = {
+            project_id: project_id,
+            items_name: $('name_item').value,
+            items_des: $('des_item').value
+        },
+        params = {
+            method: "POST",
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
+            }),
+            body: JSON.stringify(body)
+        };
     fetch("./../item/createItem", params)
-    .then(resp => resp.json())
-    .then(data => {
-        console.log(data);
-        if (data.status==200){
-            location.reload();
-        }else{
-            alert("Error al iniciar sesion, status:"+data.status);
-        }
-    });
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            if (data.status == 200) {
+                location.reload();
+            } else {
+                alert("Error al iniciar sesion, status:" + data.status);
+            }
+        });
 }
 
-function delegation(){
-    //Haz lo que te de la gana
+function delegation() {
+    let slaveUser = $('user_delegado').value;
+    let id = $('project_id_delegado').value;
+    console.log(slaveUser);
+    console.log(id);
+    let json = {
+        slaveUser: slaveUser,
+        projectId: id
+    };
+    let body = JSON.stringify(json);
+    fetch('./../delegate/give', {
+            body: body,
+            method: 'PUT',
+            headers: new Headers({
+                "Content-Type": 'application/json',
+                "Authorization": "Bearer " + sessionStorage.getItem("token")
+            }),
+        }).then(resp => resp.json())
+        .then((data) => {
+            if (data.status === 200) {
+                location.href = "dashboard.html";
+            } else {
+                location.reload();
+            }
+        })
 }
 
-$('modaldelegation').addEventListener('click',delegation);
-$('modalbtn').addEventListener('click',createItem);
-$('modalcontent').addEventListener('click',addContent);
+$('modaldelegation').addEventListener('click', delegation);
+$('modalbtn').addEventListener('click', createItem);
+$('modalcontent').addEventListener('click', addContent);
